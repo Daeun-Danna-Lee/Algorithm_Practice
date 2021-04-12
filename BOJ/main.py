4f5ee25906f7c60828eb3377e105b8b9e1cb57cb
Danna Lee <daeunlee0720@gmail.com>
2021-04-12 20:16:58 +0900

    1929 91% 틀렸습니다

--- BOJ/main.py
+++ BOJ/main.py
@@ -4,13 +4,12 @@ import sys
 input = sys.stdin.readline
 
 m, n = map(int, input().split())
-primes = []
 
 for i in range(m, n+1):
     cnt = 0
     for j in range(2, int(i**0.5)+1):
         if i % j == 0:
             cnt += 1
+            break
     if cnt == 0:
-        primes.append(i)
         print(i)
