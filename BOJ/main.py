aa522e7a922658f61abaca244bfa54bf68176604
Danna Lee <daeunlee0720@gmail.com>
2021-04-22 00:04:56 +0900

    1759

--- BOJ/main.py
+++ BOJ/main.py
@@ -8,10 +8,11 @@ vowels = ['a', 'e', 'i', 'o', 'u']
 l, c = map(int, input().split())
 words = sorted(input().split())
 
+answer = []
 for pw in combinations(words, l):
-    char_cnt = 0
+    vowel_cnt = 0
     for char in pw:
         if char in vowels:
-            char_cnt += 1
-    if char_cnt >= 1 and len(words) - 2:
-        print(''.join(pw)) # 조합된 걸 문자열로 변환
+            vowel_cnt += 1
+    if vowel_cnt >= 1 and vowel_cnt <= l - 2:
+        print(''.join(pw))
