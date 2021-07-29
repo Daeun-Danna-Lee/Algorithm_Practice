950b99edcb5116a4805294df0e4ebfa12b433a1d
Danna <daeunlee0720@gmail.com>
2021-07-29 19:30:32 +0900

    17626 맞았습니다
    사실 구글링 참고 많이 했음

--- BOJ/BOJ/17626.py
+++ BOJ/BOJ/17626.py
@@ -1,39 +1,20 @@
-import sys
+N = int(input())
+min_sum = 4 
 
-input = sys.stdin.readline
-
-squares = [0, 1, 4, 9, 16, 25]
-
-n = int(input())
-
-curr_num = 0
-curr_square = 0
-
-# DP를 위한 리스트를 만드는 로직
-if n > 25:
-    curr_num = 5
-    curr_square = 25
-    while True:
-        curr_num += 1
-        curr_square = curr_num ** 2
-
-        if curr_square > n:
-            break
-
-        squares.append(curr_square)
-
-        
-count = 0
-for i in range(len(squares)-1, 0, -1):
-    if squares[i] > n:
-        del squares[i]
+for a in range(int(N**0.5), int((N//4)**0.5), -1): 
+    if a*a == N:
+        min_sum = 1 
+        break
     else:
-        print("squares[{}]: {}".format(i, squares[i]))
-        count += 1
-        n -= squares[i]
-        print("current n:", n)
-
-if n == 1:
-    count += 1
-
-print(count)
+        temp = N - a*a
+        for b in range(int(temp**0.5), int((temp//3)**0.5), -1): 
+            if a*a + b*b == N:
+                min_sum = min(min_sum, 2)
+                continue
+            else:
+                temp = N - a*a - b*b
+                for c in range(int(temp**0.5), int((temp//2)**0.5), -1):
+                    if N == a*a + b*b + c*c:
+                        min_sum = min(min_sum, 3)
+                
+print(min_sum)
