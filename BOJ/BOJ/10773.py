26145d46801d59df150ae4c1d28fd9a21706dadb
Danna <daeunlee0720@gmail.com>
2021-08-04 17:18:15 +0900

    10773 맞았습니다

---
+++ BOJ/BOJ/10773.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+stack = []
+
+k = int(input())
+
+for _ in range(k):
+    n = int(input())
+
+    if n != 0:
+        stack.append(n)
+    else:
+        stack.pop()
+
+print(sum(stack))
